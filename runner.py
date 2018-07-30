0acd0a4789ec73fe8379b4e6740f3c24b1d57d91
Jay D <29253772+hotswapster@users.noreply.github.com>
2018-07-30 14:42:51 +1000

    update

--- runner.py
+++ runner.py
@@ -20,7 +20,7 @@ middleFrame.pack(side=TOP)
 bottomFrame = Frame(root, bg="green")
 bottomFrame.pack(side=BOTTOM)
 
-topFrame_Label = Label(topFrame, text="Runner", )
+topFrame_Label = Label(topFrame, text="Runner")
 topFrame_Label.pack()
 
 middleFrame_Label = Label(middleFrame, text="Updates HASS configurations at the click of a button", )
@@ -29,18 +29,29 @@ middleFrame_Label.pack()
 bottomFrame_Label = Label(middleFrame, text="Click to deploy")
 bottomFrame_Label.pack()
 
-configButton = Button(middleFrame, text="Button", fg="red", bg="black")
-configButton.pack()
+configButton = Button(middleFrame, text="Send Config", fg="red", bg="black")
+configButton.pack(side="top")
 
-groupsButton = Button(middleFrame, text="Button", fg="red", bg="black")
-groupsButton.pack()
+groupsButton = Button(middleFrame, text="Send Groups", fg="red", bg="black")
+groupsButton.pack(side="left")
 
-sensorsButton = Button(middleFrame, text="Button", fg="red", bg="black")
-sensorsButton.pack()
+sensorsButton = Button(middleFrame, text="Send Sensors", fg="red", bg="black")
+sensorsButton.pack(side="right")
 
-allButton = Button(middleFrame, text="Button", fg="red", bg="black")
-allButton.pack()
+allButton = Button(bottomFrame, text="Send All", fg="red", bg="black")
+allButton.pack(side="top")
+
+server = "0.0.0.0"
+rootdirectory = "/homeassistant/.homeassistant"
 
 
 root.minsize(400, 400)
 root.mainloop()
+
+doowoop = Tk()
+
+topFrame = Frame(doowoop)
+topFrame.pack(side=TOP)
+
+doowoop.minsize(400, 400)
+doowoop.mainloop()
